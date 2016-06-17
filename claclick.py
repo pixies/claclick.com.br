dc075929fa3ddd04bca287e58a6c8d29436453fd
Cleyton Fábio <pixies@pelivre.org>
2016-06-17 00:49:29 -0300

    erros corrigidos...

--- claclick.py
+++ claclick.py
@@ -21,5 +21,8 @@ def consulta():
 		)
 
 if __name__=='__main__':
-	app.run(debug=True)
-
+	app.run(
+		debug=True,
+		host='192.168.1.8',
+		port=5000
+		)
--- controle.py
+++ controle.py
@@ -14,6 +14,12 @@ def trata_headers():
     res = Response(request).response.environ
     date,time = str(datetime.now()).split(' ')
 
+    print '-----'
+    print res['SERVER_NAME']
+    print res['REMOTE_ADDR']
+    print res['HTTP_USER_AGENT'].split(';')[1]
+    print '-----'
+
     response_dic = {
         'request_method': res['REQUEST_METHOD'],
         'path_info': res['PATH_INFO'],
@@ -23,12 +29,14 @@ def trata_headers():
         'server_port': res['SERVER_PORT'],
         'http_accept': res['HTTP_ACCEPT'],
         'remote_addr': res['REMOTE_ADDR'],
-        'language': res['HTTP_ACCEPT_LANGUAGE'],
+        'language': res['HTTP_ACCEPT_LANGUAGE'].split(',')[0],
+        'so': res['HTTP_USER_AGENT'].split(';')[1],
         'date': date,
         'time': time,
     }
     return response_dic
 
 def coleta_data_of_db():
-    return registro.find()
+    date,time = str(datetime.now()).split(' ')
+    return registro.find().sort([("date",-1),("time",-1)])
     
