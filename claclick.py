c95d7488636de384bc86c28746d0b7947c375945
Cleyton Fábio <pixies@pelivre.org>
2016-06-16 22:25:36 -0300

    removendo lixo e testes

--- claclick.py
+++ claclick.py
@@ -1,9 +1,7 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 from controle import insert_data_in_db, coleta_data_of_db, trata_headers
 
 app = Flask('claclick')
-
-
 
 @app.route('/')
 def index():
@@ -11,20 +9,16 @@ def index():
 	data = trata_headers()
 	insert_data_in_db(data)
 	print 'LOG: ' + str(coleta_data_of_db())
-	
 	return redirect('https://www.facebook.com/ClaClick-272681776174594')
-	#return redirect('/n')
-	#return str(coleta_data_of_db())
 
-#	return 'feito'
-
-@app.route('/n')
-def retorne():
-	return 'foi feito <a href="/">voltar</a>'
 
 @app.route('/consulta')
 def consulta():
-	return str(coleta_data_of_db())
+	registros = coleta_data_of_db()
+	return render_template(
+		'consulta.html',
+		registros=registros
+		)
 
 if __name__=='__main__':
 	app.run(debug=True)
